cf3faa19afdcd850c1cf608c296059bc249a6f39
Michelle Bloomfield <blo16803@uvg.edu.gt>
2019-02-20 23:20:46 -0600

    changes

--- search.py
+++ search.py
@@ -3,23 +3,28 @@
 ##Autor: Michelle Bloomfield Fong 16803
 
 def graph_search(problem):
-    frontier = [Path([problem.initial])]
+  
+    frontier = []
     explored = []
+    path = []
+    estado = problem.initial
+    path.append(estado)
+    frontier.append(estado)
     
     while True:
         if len(frontier):
-            path = criteria(frontier)
+            path = problem.heuristica(frontier)
             s = path.end
             explored.append(path)
             
-            if problem.foal_test(s):
+            if problem.goaltest(s):
                 return path
             
             for a in problem.actions(s):
                 result = problem.results(s,a)
                 
                 if not is_explored(path, result, explored):
-                    new_path = Paht([])
+                    new_path = Path([])
                     naw_path.extendFrom(path)
                     new_path.addStep(problem.result(s,a))
                     frontier.append(new_path)
--- sudoku.py
+++ sudoku.py
@@ -2,161 +2,193 @@
 ##Inteligencia Artificial
 ##Autor: Michelle Bloomfield Fong 16803
 from conversiones import matrix, cuadros, linea, verificarFila, verificarColumna, verificarCuadro
+from search import graph_search
 
-entrada = input("Ingrese los numeros del sudoku:\n")
-x = matrix(entrada)
 
-def actions(s):
-    
-    valores_posibles= []
-    valores_presentes =[]
-    pos = []
-    
-    cuadrante = cuadros(s)
-    lineas = linea(s)
-    
-    uno = 0
-    dos = 0
-    tres = 0
-    cuatro = 0
-    for x in range(4):
-        for y in range(4):
-            if s[x][y] == ".":
-                pos = [x,y]
-           
-           ##CUADRANTE 1 
-    if pos[0]<=2 and pos[1]<=2:
-        nodo = lineas[y] + s[x] + cuadrante[0]
+def inicio():
+    entrada = input("Ingrese los numeros del sudoku:\n")
+    problem = sudoku()
+    problem.initial = matrix(entrada)
+    solucion = graph_search(problem)
 
-        ##calcula los valores existentes que estan en los cuadrantes, lineas y columnas
-        for n in nodo:
-            if n not in valores_presentes:
-                valores_presentes.append(n)
+class sudoku:
+    
+    initial = "estado inicial"
+    
+    def actions(self,s):
         
-        ##Verifica el numero que no existe      
-        for a in range(len(valores_presentes)):
-            if valores_presentes[a] == '1':
-                uno+=1
-            if valores_presentes[a] == '2':
-                dos+=1
-            if valores_presentes[a] == '3':
-                tres+=1
-            if valores_presentes[a] == '4':
-                cuatro+=1
-               
-        if uno == 0:
-            valores_posibles.append('1')
-        if dos == 0:
-            valores_posibles.append('2')
-        if tres == 0:
-            valores_posibles.append('3')
-        if cuatro == 0:
-            valores_posibles.append('4')
-                
-            ##CUADRANTE 2
-    if pos[0] >=2 and pos[1]<=2:
-        nodo = lineas[y] + s[x] + cuadrante[3]
+        valores_posibles= []
+        valores_presentes =[]
+        pos = []
         
-        ##calcula los valores existentes que estan en los cuadrantes, lineas y columnas
-        for n in nodo:
-            if n not in valores_presentes:
-                valores_presentes.append(n)
+        cuadrante = cuadros(s)
+        lineas = linea(s)
         
-        ##Verifica el numero que no existe 
-        for a in range(len(valores_presentes)):
-            if valores_presentes[a] == '1':
-                uno+=1
-            if valores_presentes[a] == '2':
-                dos+=1
-            if valores_presentes[a] == '3':
-                tres+=1
-            if valores_presentes[a] == '4':
-                cuatro+=1
-               
-        if uno == 0:
-            valores_posibles.append('1')
-        if dos == 0:
-            valores_posibles.append('2')
-        if tres == 0:
-            valores_posibles.append('3')
-        if cuatro == 0:
-            valores_posibles.append('4')
+        uno = 0
+        dos = 0
+        tres = 0
+        cuatro = 0
+        for x in range(4):
+            for y in range(4):
+                if s[x][y] == ".":
+                    pos = [x,y]
+               ##CUADRANTE 1 
+        if pos[0]<=2 and pos[1]<=2:
+            nodo = lineas[y] + s[x] + cuadrante[0]
+
+            ##calcula los valores existentes que estan en los cuadrantes, lineas y columnas
+            for n in nodo:
+                if n not in valores_presentes:
+                    valores_presentes.append(n)
             
-            ##CUADRANTE 3
-    if pos[0] <=2 and pos[1]>=2:
-        nodo = lineas[y] + s[x] + cuadrante[2]
-        
-        ##calcula los valores existentes que estan en los cuadrantes, lineas y columnas
-        for n in nodo:
-            if n not in valores_presentes:
-                valores_presentes.append(n)
-                
-        ##Verifica el numero que no existe 
-        for a in range(len(valores_presentes)):
-            if valores_presentes[a] == '1':
-                uno+=1
-            if valores_presentes[a] == '2':
-                dos+=1
-            if valores_presentes[a] == '3':
-                tres+=1
-            if valores_presentes[a] == '4':
-                cuatro+=1
-               
-        if uno == 0:
-            valores_posibles.append('1')
-        if dos == 0:
-            valores_posibles.append('2')
-        if tres == 0:
-            valores_posibles.append('3')
-        if cuatro == 0:
-            valores_posibles.append('4')
+            ##Verifica el numero que no existe      
+            for a in range(len(valores_presentes)):
+                if valores_presentes[a] == '1':
+                    uno+=1
+                if valores_presentes[a] == '2':
+                    dos+=1
+                if valores_presentes[a] == '3':
+                    tres+=1
+                if valores_presentes[a] == '4':
+                    cuatro+=1
+                   
+            if uno == 0:
+                valores_posibles.append(1)
+            if dos == 0:
+                valores_posibles.append(2)
+            if tres == 0:
+                valores_posibles.append(3)
+            if cuatro == 0:
+                valores_posibles.append(4)
+                    
+                ##CUADRANTE 2
+        if pos[0] >=2 and pos[1]<=2:
+            nodo = lineas[y] + s[x] + cuadrante[3]
             
-            ##CUADRANATE 4
-    if pos[0] >=2 and pos[1]>=2:
-        nodo = lineas[y] + s[x] + cuadrante[3]
-        
-        ##calcula los valores existentes que estan en los cuadrantes, lineas y columnas
-        for n in nodo:
-            if n not in valores_presentes:
-                valores_presentes.append(n)
-           
-        ##Verifica el numero que no existe 
-        for a in range(len(valores_presentes)):
-            if valores_presentes[a] == '1':
-                uno+=1
-            if valores_presentes[a] == '2':
-                dos+=1
-            if valores_presentes[a] == '3':
-                tres+=1
-            if valores_presentes[a] == '4':
-                cuatro+=1
+            ##calcula los valores existentes que estan en los cuadrantes, lineas y columnas
+            for n in nodo:
+                if n not in valores_presentes:
+                    valores_presentes.append(n)
+            
+            ##Verifica el numero que no existe 
+            for a in range(len(valores_presentes)):
+                if valores_presentes[a] == '1':
+                    uno+=1
+                if valores_presentes[a] == '2':
+                    dos+=1
+                if valores_presentes[a] == '3':
+                    tres+=1
+                if valores_presentes[a] == '4':
+                    cuatro+=1
+                   
+            if uno == 0:
+                valores_posibles.append(1)
+            if dos == 0:
+                valores_posibles.append(2)
+            if tres == 0:
+                valores_posibles.append(3)
+            if cuatro == 0:
+                valores_posibles.append(4)
+                
+                ##CUADRANTE 3
+        if pos[0] <=2 and pos[1]>=2:
+            nodo = lineas[y] + s[x] + cuadrante[2]
+            
+            ##calcula los valores existentes que estan en los cuadrantes, lineas y columnas
+            for n in nodo:
+                if n not in valores_presentes:
+                    valores_presentes.append(n)
+                    
+            ##Verifica el numero que no existe 
+            for a in range(len(valores_presentes)):
+                if valores_presentes[a] == '1':
+                    uno+=1
+                if valores_presentes[a] == '2':
+                    dos+=1
+                if valores_presentes[a] == '3':
+                    tres+=1
+                if valores_presentes[a] == '4':
+                    cuatro+=1
+                   
+            if uno == 0:
+                valores_posibles.append(1)
+            if dos == 0:
+                valores_posibles.append(2)
+            if tres == 0:
+                valores_posibles.append(3)
+            if cuatro == 0:
+                valores_posibles.append(4)
+                
+                ##CUADRANATE 4
+        if pos[0] >=2 and pos[1]>=2:
+            nodo = lineas[y] + s[x] + cuadrante[3]
+            
+            ##calcula los valores existentes que estan en los cuadrantes, lineas y columnas
+            for n in nodo:
+                if n not in valores_presentes:
+                    valores_presentes.append(n)
                
-        if uno == 0:
-            valores_posibles.append('1')
-        if dos == 0:
-            valores_posibles.append('2')
-        if tres == 0:
-            valores_posibles.append('3')
-        if cuatro == 0:
-            valores_posibles.append('4')
-  
+            ##Verifica el numero que no existe 
+            for a in range(len(valores_presentes)):
+                if valores_presentes[a] == '1':
+                    uno+=1
+                if valores_presentes[a] == '2':
+                    dos+=1
+                if valores_presentes[a] == '3':
+                    tres+=1
+                if valores_presentes[a] == '4':
+                    cuatro+=1
+                   
+            if uno == 0:
+                valores_posibles.append(1)
+            if dos == 0:
+                valores_posibles.append(2)
+            if tres == 0:
+                valores_posibles.append(3)
+            if cuatro == 0:
+                valores_posibles.append(4)
+            
+            return valores_posibles
 
-def results(a, s):
-    return
-
-
-##Defini la lista ganadora y se compara
-def goalTest(s):
-        ganador = [['1','2','3','4'], ['5','6','7','8'], ['9','A','B','C'],['D','E','F','.']]
-        if s == ganador:
-            return True
-        else:
-            return False
-
-def stepCoast(s, a, i):
-    return
-
-def pathCoast():
-    return
+    def result(self,s, a):
         
-actions(x)
+        s_nueva = 0
+        for x in range(4):
+            for y in range(4):
+                if s[x][y] == ".":
+                    pos = [x,y]
+                    
+        for n in range(len(a)):
+            s[pos[0]][pos[1]] = str(a[n])
+            s_nueva.append(s)
+            
+        print(s_nueva)
+        return s_nueva
+
+
+    ##Defini la lista ganadora y se compara
+    def goalTest(self,s):
+            for x in range(4):
+                for y in range(4):
+                    if s[x][y] == ".":
+                        return False
+                    else:
+                        return True
+
+    def stepCoast(self, s, a, s_nueva):
+        return 1 
+        
+    def pathCoast(self,s_nueva):
+        return len(s_nueva)
+
+    def heuristica(self,s):
+        a = 0
+        for n in range(len[s]):
+            new = s[n]
+            for x in range(4):
+                for y in range(4):
+                    if new[x][y] == ".":
+                        a+=1;
+            return a
+            
+inicio()
